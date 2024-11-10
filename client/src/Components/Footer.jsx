f4420c8ce46380a14576ca355cc48920e40db038
subham007-coder <dsubham109@gmail.com>
2024-11-10 22:24:45 +0530

    nothing add value

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="bg-slate-950 w-full px-10 py-2 flex tracking-tighter overflow-hidden text-[.8rem]">
+    <div className="bg-slate-950 w-full px-10 py-2 flex tracking-tighter justify-center items-center overflow-hidden text-[.8rem]">
       <img
         className="w-10"
         src="https://cdn3d.iconscout.com/3d/premium/thumb/video-call-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--logo-conference-chat-communication-business-training-pack-illustrations-3061900.png?f=webp"
